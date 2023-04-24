b64a2210d923a89e48b837d9e1ecbf9480e96f06
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-04-24 03:32:56 +0200

    dw123

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/", methods=["POST"])
+@app.route("/download", methods=["POST"])
 def download():
     url = request.form.get("url")
     if not url:
